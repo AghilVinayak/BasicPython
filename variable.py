e2838dece8a8261af23a7d3e7cd0becf76c8ab7b
aghilvinayak.p@gmail.com <aghilvinayak.p@gmail.com>
2020-05-31 01:11:01 +0530

    string related functions introduced

--- variable.py
+++ variable.py
@@ -7,3 +7,7 @@ print("There once was a man named " + character_name + ",")
 print("he was " + character_age + " years old. ")
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
+
+phrase = "Giraffe Academy"
+print(character_name .index("h"))
+print(phrase .replace("Giraffe", "Elephant"))
