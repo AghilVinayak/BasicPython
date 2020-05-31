383c3787a1ecb53c87211c2572f82a53523fc36d
Aghil Vinayak <aghilvinayak.p@gmail.com>
2020-05-31 12:52:49 +0530

    basic functions using numbers

---
+++ numbers.py
@@ -0,0 +1,19 @@
+print(2)
+print(2.0987)
+print(-2.0987)
+print(3+4.5)
+print(3*4+5)
+print(3*(4+5))
+print(10%3)
+
+my_num = 5
+print(str(my_num) + " my favorite number")
+
+my_num = -5
+print(abs(my_num))
+print(pow(3,2))
+print(pow(4,6))
+print(min(4,6))
+print(round(3.4))
+print(round(3.7))
+
